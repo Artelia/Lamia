382f816ffaa1cee708db83c6026703354512d21f
patricev <patverch@yahoo.fr>
2019-10-03 18:27:03 +0200

    test

--- docshpinx/deploy.bat
+++ docshpinx/deploy.bat
@@ -1,16 +1,23 @@
-del /Q %~dp0\source\images
-del %~dp0\source\*.rst
+::del /Q %~dp0\source\images
+::del %~dp0\source\*.rst
+::
+::python %~dp0\odt2sphinx\odt2sphinx.py documentation.odt %~dp0\source
+::REM odt2sphinx documentation.odt %~dp0\source
+::
+::del /Q %~dp0\build\html
+::
+::call  makehtml.bat
+::
+::@echo Start copy to Lamia\doc_html ...
+::xcopy /E /Y /S /I /Q %~dp0\build\html %~dp0\..\Lamia\doc_html
+::
+::xcopy /E /Y /S /I /Q %~dp0\build\html %~dp0\..\\docs
+::
+::
+::
+::@echo Finish copy to Lamia\doc_html
 
-python %~dp0\odt2sphinx\odt2sphinx.py documentation.odt %~dp0\source
-REM odt2sphinx documentation.odt %~dp0\source
-
-del /Q %~dp0\build\html
-
-call  makehtml.bat
-
-@echo Start copy to Lamia\doc_html ...
-xcopy /E /Y /S /I /Q %~dp0\build\html %~dp0\..\Lamia\doc_html
-@echo Finish copy to Lamia\doc_html
+python htmlpars.py
 
 
 
