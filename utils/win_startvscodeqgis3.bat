246fe302a65a61c3a11a2a4b52fd352b1e30c8f3
patricev <patrice.verchere@arteliagroup.com>
2020-06-29 09:13:02 +0200

    bug fix

--- utils/win_startvscodeqgis3.bat
+++ utils/win_startvscodeqgis3.bat
@@ -1,5 +1,8 @@
 call %~dp0/win_setqgisenv.bat
 
+path %PATH%;C:\Program Files\Git\cmd;C:\Program Files\Docker\Docker\resources\bin;C:\Users\patrice.verchere\AppData\Local\Programs\Git\cmd;C:\ProgramData\DockerDesktop\version-bin
+
+
 REM save my life !!!
 SET QGIS_DISABLE_MESSAGE_HOOKS=1
 SET QGIS_NO_OVERRIDE_IMPORT=1
