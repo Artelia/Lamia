9c20cefb889867d13cb7478f0d4bdc901cafe8c4
patricev <patrice.verchere@arteliagroup.com>
2020-07-02 16:33:17 +0200

    temp

--- utils/win_setqgisenv.bat
+++ utils/win_setqgisenv.bat
@@ -10,11 +10,11 @@ call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
 call py3_env.bat
 call qt5_env.bat
 path %QGISPATH%\bin;%PATH%
-path %UserProfile%\AppData\Roaming\Python\Python37\Scripts;%PATH%
+REM path %UserProfile%\AppData\Roaming\Python\Python37\Scripts;%PATH%
 set QGIS_PREFIX_PATH=%QGISPATH%
 set QT_PLUGIN_PATH=%QGISPATH%\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
 set PYTHONPATH=%QGISPATH%\python;%PYTHONPATH%
-set PYTHONPATH=%UserProfile%\AppData\Roaming\python\Python37\site-packages;%PYTHONPATH%
+REM set PYTHONPATH=%UserProfile%\AppData\Roaming\python\Python37\site-packages;%PYTHONPATH%
 
 REM set PYTHONPATH=%PYTHONPATH%;%QGISPATH%\python;
 REM set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\Python37\lib\site-packages
--- utils/win_startvscodeqgis3.bat
+++ utils/win_startvscodeqgis3.bat
@@ -2,6 +2,9 @@ call %~dp0/win_setqgisenv.bat
 
 path %PATH%;C:\Program Files\Git\cmd;C:\Program Files\Docker\Docker\resources\bin;C:\Users\patrice.verchere\AppData\Local\Programs\Git\cmd;C:\ProgramData\DockerDesktop\version-bin
 
+REM pip --user for vscode : black, sphinx, sphonx rtd theme
+path %UserProfile%\AppData\Roaming\Python\Python37\Scripts;%PATH%
+set PYTHONPATH=%UserProfile%\AppData\Roaming\python\Python37\site-packages;%PYTHONPATH%
 
 REM save my life !!!
 SET QGIS_DISABLE_MESSAGE_HOOKS=1
