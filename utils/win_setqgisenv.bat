6b56c8bfe5b483783ef7d0e05f9f8b625ec3d598
patricev <patrice.verchere@arteliagroup.com>
2020-07-02 16:29:12 +0200

    config

--- utils/win_setqgisenv.bat
+++ utils/win_setqgisenv.bat
@@ -10,7 +10,7 @@ call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
 call py3_env.bat
 call qt5_env.bat
 path %QGISPATH%\bin;%PATH%
-path %UserProfile%\AppData\Roaming\Python\Python37\Scripts;%PATH%
+REM path %UserProfile%\AppData\Roaming\Python\Python37\Scripts;%PATH%
 set QGIS_PREFIX_PATH=%QGISPATH%
 set QT_PLUGIN_PATH=%QGISPATH%\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
 set PYTHONPATH=%QGISPATH%\python;%PYTHONPATH%
--- utils/win_startvscodeqgis3.bat
+++ utils/win_startvscodeqgis3.bat
@@ -1,7 +1,13 @@
 call %~dp0/win_setqgisenv.bat
 
+REM specific vscode libs/path
+
 path C:\Program Files\Git\cmd;%UserProfile%\AppData\Local\Programs\Git\cmd;%PATH%
 path C:\Program Files\Docker\Docker\resources\bin;C:\ProgramData\DockerDesktop\version-bin;%PATH%
+
+REM pip --user for vscode : black, sphinx, sphonx rtd theme
+path %UserProfile%\AppData\Roaming\Python\Python37\Scripts;%PATH%
+set PYTHONPATH=%UserProfile%\AppData\Roaming\python\Python37\site-packages;%PYTHONPATH%
 
 
 REM save my life !!!
