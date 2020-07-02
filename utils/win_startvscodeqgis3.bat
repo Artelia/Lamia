91d164ea65f54c4e664c559dd2bf24c1b9c8ec63
patricev <patrice.verchere@arteliagroup.com>
2020-07-02 16:39:23 +0200

    popo

--- utils/win_startvscodeqgis3.bat
+++ utils/win_startvscodeqgis3.bat
@@ -11,4 +11,4 @@ SET QGIS_DISABLE_MESSAGE_HOOKS=1
 SET QGIS_NO_OVERRIDE_IMPORT=1
 SET QT_LOGGING_RULES=*.debug=false;*.warning=false
 
-start "PyCharm aware of Quantum GIS" /B "%UserProfile%\AppData\Local\Programs\Microsoft VS Code\Code.exe" %*
+start "VSCode aware of Quantum GIS" /B "%UserProfile%\AppData\Local\Programs\Microsoft VS Code\Code.exe" %*
